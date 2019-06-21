3e741bc90b44b6eb3f6abd5cf9dac7a77f1d47e1
Lucas Pellison <lucaspellison@gmail.com>
2019-06-21 11:57:05 -0300

    more flexibility

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,19 @@
 const router = require('express').Router()
 
+const bodyParser = require('body-parser');
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: false }));
+
 const messages = []
 
 router.get("/", (req, res) => {
     res.status(200).send(messages)
 })
 
-router.post("/message/:nickname/:msg", async (req, res) => {
-    const sendBack = `message '${req.params.msg}' as been sended by '${req.params.nickname}'`
-    await messages.push({ nickname: req.params.nickname, msg: req.params.msg, date: Date() })
+router.post("/message/:nickname", async (req, res) => {
+    const sendBack = `message '${req.body.msg}' as been sended by '${req.params.nickname}'`
+    await messages.push({ nickname: req.params.nickname, msg: req.body.msg, date: req.body.date })
     res.status(201).send(sendBack)
     req.io.emit('newMessage', messages);
 })
